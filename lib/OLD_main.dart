efa7822ebd29185193402abf53bf91c0cd80924b
Jeff R <jruocco36@gmail.com>
2020-04-24 17:46:12 -0400

    program name input validation, new main.dart for stateful widgets

--- lib/OLD_main.dart
+++ lib/OLD_main.dart
@@ -8,67 +8,18 @@ import './test.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+// StatelessWidget - doesn't change/not re-rendered on user input
+// StatefulWidget - changes/re-rendered on user input
+class MyApp extends StatefulWidget {
+  // Link this StatefulWidget class to State class
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Program creator',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primaryColor: Color(0xFF484850),
-        primaryColorLight: Color(0xFF484850),
-        primaryColorDark: Color(0xFF000000),
-        accentColor: Color(0xFFd37c7c),
-        // hintColor: Color(0xFF484850),
-        textTheme: TextTheme(
-          display1: TextStyle(
-            color: Color(0xFFffffff),
-          ),
-          body1: TextStyle(
-            color: Color(0xFFffffff),
-          ),
-          body2: TextStyle(
-            color: Color(0xFFffffff),
-          ),
-          button: TextStyle(
-            color: Color(0xFFffffff),
-          ),
-        ),
-        canvasColor: Color(0xFF212128),
-      ),
-      home: MyHomePage(title: 'Programs'),
-    );
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MyAppState();
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
+class _MyAppState extends State<MyApp> {
   Map<int, String> _programs = {};
   var _programIndex = 1;
   // TextEditingController _textFieldController = TextEditingController();
@@ -153,20 +104,20 @@ class _MyHomePageState extends State<MyHomePage> {
         // ),
         floatingActionButton: FloatingActionButton(
           onPressed: () {
-            // print('DIALOG');
-            // showDialog(context: context, builder: (_) {
-            //   return MyDialog();
-            // });
-            showDialog(
-                context: context,
-                builder: (_) {
-                  return NewProgramButton(
-                    newProgram: _newProgram,
-                    programNames: (_programs.keys).map((program) {
-                      return _programs[program];
-                    }).toList(),
-                  );
-                });
+            print('DIALOG');
+            showDialog(context: context, builder: (_) {
+              return MyDialog();
+            });
+            // showDialog(
+            //     context: context,
+            //     builder: (_) {
+            //       return NewProgramButton(
+            //         newProgram: _newProgram,
+            //         programNames: (_programs.keys).map((program) {
+            //           return _programs[program];
+            //         }).toList(),
+            //       );
+            //     });
           },
           child: Icon(Icons.add),
           elevation: 0,
--- lib/newProgramButton.dart
+++ lib/newProgramButton.dart
@@ -7,123 +7,125 @@ class NewProgramButton extends StatefulWidget {
   NewProgramButton({this.newProgram, this.programNames});
 
   @override
-  _NewProgramButtonState createState() => _NewProgramButtonState(
-      newProgram: newProgram, programNames: programNames);
+  _NewProgramButtonState createState() => _NewProgramButtonState();
 }
 
 class _NewProgramButtonState extends State<NewProgramButton> {
   TextEditingController _textFieldController = TextEditingController();
   String _dropdownValue = 'Cycle based';
-  final Function newProgram;
-  List<String> programNames;
   String _errorText;
+  bool _nameError = false;
 
-  _NewProgramButtonState({this.newProgram, this.programNames});
-
-  _displayDialog(BuildContext context) async {
-    return await showDialog(
-        context: context,
-        builder: (context) {
-          return AlertDialog(
-            contentTextStyle: TextStyle(color: Color(0xFF888888)),
-            elevation: 0,
-            title: Text(
-              'Create program',
-              style: TextStyle(color: Colors.white),
-            ),
-            backgroundColor: Color(0xFF484850),
-            content: StatefulBuilder(
-                builder: (BuildContext context, StateSetter setState) {
-              return SingleChildScrollView(
-                child: ListBody(children: <Widget>[
-                  Container(
-                    color: Colors.transparent,
-                    child: Text('Program name:'),
-                  ),
-                  TextField(
-                    autofocus: true,
-                    cursorColor: Colors.white,
-                    controller: _textFieldController,
-                    style: TextStyle(color: Colors.white),
-                    decoration: InputDecoration(
-                      // hintText: 'Program name',
-                      errorText: _errorText,
-                      focusedBorder: UnderlineInputBorder(
-                        borderSide: BorderSide(
-                          width: 1,
-                          color: Color(0xFF888888),
-                        ),
-                      ),
-                    ),
-                  ),
-                  Container(
-                    padding: EdgeInsets.only(top: 10),
-                    child: Text('Type of program:'),
-                  ),
-                  DropdownButton(
-                    value: _dropdownValue,
-                    elevation: 0,
-                    isExpanded: true,
-                    style: TextStyle(
-                      color: Colors.white,
-                    ),
-                    items: <String>['Cycle based', 'Day based']
-                        .map<DropdownMenuItem<String>>((String value) {
-                      return DropdownMenuItem<String>(
-                        value: value,
-                        child: Text(value),
-                      );
-                    }).toList(),
-                    onChanged: (String newValue) {
-                      setState(() => _dropdownValue = newValue);
-                    },
-                  )
-                ]),
-              );
-            }),
-            actions: <Widget>[
-              FlatButton(
-                child: Text(
-                  'CANCEL',
-                  style: TextStyle(color: Colors.white),
-                ),
-                onPressed: () {
-                  Navigator.of(context).pop();
-                },
-              ),
-              FlatButton(
-                color: Color(0xFFD37C7C),
-                child: Text('OK'),
-                // TODO: validate input
-                onPressed: () {
-                  print(programNames.toString());
-                  print(_textFieldController.text);
-                  if (programNames.contains(_textFieldController.text)) {
-                    _errorText = 'Program name ' +
-                        _textFieldController.text +
-                        ' already exists.';
-                    return;
-                  }
-                  if (_textFieldController.text.length > 0)
-                    newProgram(_textFieldController.text, _dropdownValue);
-                  Navigator.of(context).pop();
-                },
-              ),
-            ],
-          );
-        }).then((val) {
-      _textFieldController.text = '';
-      _dropdownValue = 'Cycle based';
-    });
+  void clearDialog() {
+    _textFieldController.text = '';
+    _dropdownValue = 'Cycle based';
+    // _errorText = null;
+    _nameError = false;
   }
 
   @override
   Widget build(BuildContext context) {
-    return FloatingActionButton(
-      onPressed: () => _displayDialog(context),
-      child: Icon(Icons.add),
+    return AlertDialog(
+      contentTextStyle: TextStyle(color: Color(0xFF888888)),
       elevation: 0,
-      // backgroundColor: Color(0xFFD37C7C),
+      title: Text(
+        'Create program',
+        style: TextStyle(color: Colors.white),
+      ),
+      backgroundColor: Color(0xFF484850),
+      content:
+          // StatefulBuilder(
+          // builder: (BuildContext context, StateSetter setState) {
+          SingleChildScrollView(
+        child: ListBody(children: <Widget>[
+          Container(
+            color: Colors.transparent,
+            child: Text('Program name:'),
+          ),
+          TextField(
+            autofocus: true,
+            cursorColor: Colors.white,
+            controller: _textFieldController,
+            style: TextStyle(color: Colors.white),
+            decoration: InputDecoration(
+              // hintText: 'Program name',
+              errorText: _nameError ? _errorText : null,
+              focusedBorder: UnderlineInputBorder(
+                borderSide: BorderSide(
+                  width: 1,
+                  color: Color(0xFF888888),
+                ),
+              ),
+            ),
+            onChanged: (String text) {},
+          ),
+          Container(
+            padding: EdgeInsets.only(top: 10),
+            child: Text('Type of program:'),
+          ),
+          DropdownButton(
+            value: _dropdownValue,
+            elevation: 0,
+            isExpanded: true,
+            style: TextStyle(
+              color: Colors.white,
+            ),
+            items: <String>['Cycle based', 'Day based']
+                .map<DropdownMenuItem<String>>((String value) {
+              return DropdownMenuItem<String>(
+                value: value,
+                child: Text(value),
+              );
+            }).toList(),
+            onChanged: (String newValue) {
+              setState(() => _dropdownValue = newValue);
+            },
+          )
+        ]),
+      ),
+      // }),
+      actions: <Widget>[
+        FlatButton(
+          child: Text(
+            'CANCEL',
+            style: TextStyle(color: Colors.white),
+          ),
+          onPressed: () {
+            clearDialog();
+            Navigator.of(context).pop();
+          },
+        ),
+        FlatButton(
+          color: Color(0xFFD37C7C),
+          child: Text('OK'),
+          // TODO: validate input
+          onPressed: () {
+            if (widget.programNames.contains(_textFieldController.text)) {
+              setState(() {
+                _nameError = true;
+                _errorText = 'Program name ' +
+                _textFieldController.text +
+                ' already exists.';
+                return;
+              });
+            } else if (_textFieldController.text.length < 1) {
+              setState(() {
+                _nameError = true;
+                _errorText = 'Must enter program name';
+                return;
+              });
+            } else {
+              widget.newProgram(_textFieldController.text, _dropdownValue);
+              Navigator.of(context).pop();
+            }
+          },
+        ),
+      ],
     );
   }
+}
+
+class ProgramNameField extends StatefulWidget {
+  @override
+  _NewProgramButtonState createState() => new _NewProgramButtonState();
 }

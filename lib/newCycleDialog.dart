1c05bca9bb5d377313fca690a40770ed5fce7b3a
Jeff R <jruocco36@gmail.com>
2020-04-25 19:54:59 -0400

    display picked date in new cycle dialog

--- lib/newCycleDialog.dart
+++ lib/newCycleDialog.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:intl/intl.dart';
+
 import './global.dart';
 
 class NewCycleDialog extends StatefulWidget {
@@ -19,17 +21,23 @@ class _NewCycleDialogState extends State<NewCycleDialog> {
   TextEditingController _startDateController = TextEditingController();
   TextEditingController _cycleNameController = TextEditingController();
   TextEditingController _tmPercentController = TextEditingController();
-  String _dropdownValue = '1 Rep Max';
-  String _errorText;
+  String _cycleTypeDropdown = '1 Rep Max';
   bool _dateError = false;
   bool _nameError = false;
   bool _tmError = false;
+  String _dateErrorText;
+  String _nameErrorText;
+  String _tmErrorText;
 
   void clearDialog() {
     _cycleNameController.text = null;
-    _dropdownValue = '1 Rep Max';
-    _errorText = null;
+    _cycleTypeDropdown = '1 Rep Max';
+    _dateError = false;
     _nameError = false;
+    _tmError = false;
+    _dateErrorText = null;
+    _nameErrorText = null;
+    _tmErrorText = null;
   }
 
   @override
@@ -48,114 +56,147 @@ class _NewCycleDialogState extends State<NewCycleDialog> {
         style: dialogTextStyle,
       ),
       content: SingleChildScrollView(
-        child: ListBody(children: <Widget>[
-          Container(
-            child: Text('Start date:'),
-          ),
-          TextField(
-              autofocus: true,
-              cursorColor: Colors.white,
-              controller: _startDateController,
-              style: dialogTextStyle,
-              decoration: InputDecoration(
-                errorText: _dateError ? _errorText : null,
-              ),
-              onChanged: (String text) {
-                setState(() {
-                  _dateError = false;
-                });
-              },
-              onTap: () {
-                FocusScope.of(context).requestFocus(new FocusNode());
+        child: ListBody(
+          children: <Widget>[
+            // Start Date Input
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Container(
+                  padding: EdgeInsets.only(top: 10),
+                  child: Text('Start date:'),
+                ),
+                TextField(
+                    autofocus: true,
+                    cursorColor: Colors.white,
+                    controller: _startDateController,
+                    style: dialogTextStyle,
+                    decoration: InputDecoration(
+                      errorText: _dateError ? _dateErrorText : null,
+                    ),
+                    onChanged: (String text) {
+                      setState(() {
+                        _dateError = false;
+                      });
+                    },
+                    onTap: () async {
+                      FocusScope.of(context).requestFocus(new FocusNode());
 
-                Future<DateTime> selectedDate = showDatePicker(
-                  context: context,
-                  initialDate: DateTime.now(),
-                  firstDate: DateTime.now().subtract(Duration(days: 365)),
-                  lastDate: DateTime.now().add(Duration(days: 365)),
-                );
+                      DateTime selectedDate = await showDatePicker(
+                        context: context,
+                        initialDate: DateTime.now(),
+                        firstDate: DateTime.now().subtract(Duration(days: 365)),
+                        lastDate: DateTime.now().add(Duration(days: 365)),
+                      );
 
-                // TODO: figure out how to display date as string
+                      print(selectedDate);
+                      if (selectedDate != null) {
+                        setState(() {
+                          _startDateController.text =
+                              DateFormat('MM/dd/yyyy').format(selectedDate);
+                          _dateError = false;
+                        });
+                      }
 
-                print(selectedDate.toString());
+                      // selectedDate.whenComplete(() => {
+                      //       if (selectedDate != null)
+                      //         {
+                      //           setState(() {
+                      //             _startDateController.text = selectedDate.toString();
+                      //           })
+                      //         }
+                      //     });
 
-                selectedDate.whenComplete(() => {
-                  if (selectedDate != null) {
+                      //     selectedDate.whenComplete(() => {
+                      //   if (selectedDate != null) setState(() {
+                      //     // _startDateController.text = selectedDate.toString();
+                      //   }
+
+                      //     },
+                      // },
+                    }),
+              ],
+            ),
+            // Cycle Name Input
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Container(
+                  padding: EdgeInsets.only(top: 10),
+                  child: Text('Cycle name:'),
+                ),
+                TextField(
+                  autofocus: true,
+                  cursorColor: Colors.white,
+                  controller: _cycleNameController,
+                  style: dialogTextStyle,
+                  decoration: InputDecoration(
+                    errorText: _nameError ? _nameErrorText : null,
+                  ),
+                  onChanged: (String text) {
                     setState(() {
-                      _startDateController.text = selectedDate.toString();
-                    })
-                  }
-                });
-
-                //     selectedDate.whenComplete(() => {
-                //   if (selectedDate != null) setState(() {
-                //     // _startDateController.text = selectedDate.toString();
-                //   }
-
-                //     },
-                // },
-              }),
-          Container(
-            padding: EdgeInsets.only(top: 10),
-            child: Text('Cycle name:'),
-          ),
-          TextField(
-            autofocus: true,
-            cursorColor: Colors.white,
-            controller: _cycleNameController,
-            style: dialogTextStyle,
-            decoration: InputDecoration(
-              errorText: _nameError ? _errorText : null,
+                      _nameError = false;
+                    });
+                  },
+                ),
+              ],
             ),
-            onChanged: (String text) {
-              setState(() {
-                _nameError = false;
-              });
-            },
-          ),
-          Container(
-            padding: EdgeInsets.only(top: 10),
-            child: Text('Training Max Percentage:'),
-          ),
-          TextField(
-            autofocus: true,
-            cursorColor: Colors.white,
-            controller: _tmPercentController,
-            style: dialogTextStyle,
-            decoration: InputDecoration(
-              errorText: _tmError ? _errorText : null,
-              suffix: Text(
-                '%',
-                style: dialogTextStyle,
-              ),
+            // Training Max Percentage Input
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Container(
+                  padding: EdgeInsets.only(top: 10),
+                  child: Text('Training Max Percentage:'),
+                ),
+                TextField(
+                  autofocus: true,
+                  cursorColor: Colors.white,
+                  controller: _tmPercentController,
+                  style: dialogTextStyle,
+                  decoration: InputDecoration(
+                    errorText: _tmError ? _tmErrorText : null,
+                    suffix: Text(
+                      '%',
+                      style: dialogTextStyle,
+                    ),
+                  ),
+                  onChanged: (String text) {
+                    setState(() {
+                      _tmError = false;
+                    });
+                  },
+                ),
+              ],
             ),
-            onChanged: (String text) {
-              setState(() {
-                _tmError = false;
-              });
-            },
-          ),
-          Container(
-            padding: EdgeInsets.only(top: 10),
-            child: Text('Increase 1RM or TM?'),
-          ),
-          DropdownButton(
-            value: _dropdownValue,
-            elevation: 0,
-            isExpanded: true,
-            style: dialogTextStyle,
-            items: <String>['1 Rep Max', 'Training Max']
-                .map<DropdownMenuItem<String>>((String value) {
-              return DropdownMenuItem<String>(
-                value: value,
-                child: Text(value),
-              );
-            }).toList(),
-            onChanged: (String newValue) {
-              setState(() => _dropdownValue = newValue);
-            },
-          )
-        ]),
+            // Cycle Type Input
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Container(
+                  padding: EdgeInsets.only(top: 10),
+                  child: Text('Increase 1RM or TM?'),
+                ),
+                DropdownButton(
+                  value: _cycleTypeDropdown,
+                  elevation: 0,
+                  isExpanded: true,
+                  style: dialogTextStyle,
+                  items: <String>['1 Rep Max', 'Training Max']
+                      .map<DropdownMenuItem<String>>((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(value),
+                    );
+                  }).toList(),
+                  onChanged: (String newValue) {
+                    setState(() => _cycleTypeDropdown = newValue);
+                  },
+                )
+              ],
+            ),
+          ],
+        ),
       ),
       // }),
       actions: <Widget>[
@@ -173,24 +214,41 @@ class _NewCycleDialogState extends State<NewCycleDialog> {
           color: flamingoColor,
           child: Text('OK'),
           onPressed: () {
+            if (_startDateController.text.length < 1) {
+              setState(() {
+                _dateError = true;
+                _dateErrorText = 'Must enter start date';
+              });
+            }
             if (widget.cycleNames.contains(_cycleNameController.text)) {
               setState(() {
                 _nameError = true;
-                _errorText = 'Program name ' +
+                _nameErrorText = 'Cycle name ' +
                     _cycleNameController.text +
                     ' already exists.';
-                return;
               });
-            } else if (_cycleNameController.text.length < 1) {
+            }
+            if (_cycleNameController.text.length < 1) {
               setState(() {
                 _nameError = true;
-                _errorText = 'Must enter program name';
-                return;
+                _nameErrorText = 'Must enter cycle name';
               });
-            } else {
-              widget.newCycle(_cycleNameController.text, _dropdownValue);
+            }
+            if (_tmPercentController.text.length < 1) {
+              setState(() {
+                _tmError = true;
+                _tmErrorText = 'Must enter training max percentage';
+              });
+            }
+
+            if (_dateError == false &&
+                _nameError == false &&
+                _tmError == false) {
+              widget.newCycle(_cycleNameController.text, _cycleTypeDropdown);
               clearDialog();
               Navigator.of(context).pop();
+            } else {
+              return;
             }
           },
         ),
